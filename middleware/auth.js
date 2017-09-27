c7d3e9b46c852ab20e1451be02f9dbd932e5238e
nimesh1601 <nimesh1601@gmail.com>
2017-09-27 22:40:55 +0530

    First commit

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+var {User} = require('./../models/user');
+
+var auth = (req, res, next) => {
+  var token = req.header('x-autha');
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  }).catch((e) => {
+    res.status(401).send();
+  });
+};
+
+module.exports = {auth};
